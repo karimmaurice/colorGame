a9e854dc0dd11ca8b3981883b32da4668e83a8c9
Karim Maurice <karim.s.maurice@gmail.com>
2015-01-21 09:53:02 +0200

    db fixed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141223224135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "api_keys", force: true do |t|
     t.string   "key"
     t.integer  "user_id"
